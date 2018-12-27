ec4f2ef82331f367c79b4dfd7702dee4185f08f5
SeoulStore <stayfoolish24@gmail.com>
2018-12-27 18:48:13 +0900

    화면 구성 중

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import { AppRegistry } from "react-native"
+// import App from "./App"
+import App from "./src_checkpoint_01/components/Flashcards"
+import { name as appName } from "./app.json"
+
+AppRegistry.registerComponent(appName, () => App)
+
+/*
+import Flashcards1 from "./src_checkpoint_01/components/Flashcards"
+
+export default Flashcards1
+*/
